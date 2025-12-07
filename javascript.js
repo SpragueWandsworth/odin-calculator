5fea0dca70e40cabf636f8036d2d15330f76d637
SpragueWandsworth <thetelescope46@gmail.com>
2025-12-07 14:58:13 +0000

    added operate function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 
+let number1 = null;
+let operator = null;
+let number2 = null;
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -16,4 +20,20 @@ function divide(num1, num2) {
     return num1 / num2;
 };
 
-console.log(divide(20, 4));
+function operate(num1, operator, num2) {
+    
+    if (operator === '+') {
+        console.log(add(num1, num2));
+    }
+    else if (operator === '-') {
+        console.log(subtract(num1, num2));
+    }
+    else if (operator === '*') {
+        console.log(multiply(num1, num2));
+    }
+    else if (operator === '/') {
+        console.log(divide(num1, num2));
+    };
+};
+
+operate(2, '/' , 2);
